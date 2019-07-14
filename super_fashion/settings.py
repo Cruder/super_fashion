1e48dd9997ef3d297ae1414890c778523c5a0a7c
Amelie Certin <amelie.certin@gmail.com>
2019-07-14 22:56:09 +0200

    Create app

--- super_fashion/settings.py
+++ super_fashion/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'fashion.apps.FashionConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
