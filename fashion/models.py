abaaa17b055cc1641f7368b2e9cc42dc37120739
Amelie Certin <amelie.certin@gmail.com>
2019-07-15 23:50:47 +0200

    Create model

--- fashion/models.py
+++ fashion/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class FashionFile(models.Model):
+    file_name = models.CharField(max_length=200)
+    type = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.file_name + ' ' + self.type
--- fashion/views.py
+++ fashion/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from .forms import UploadFileForm
-
+from .models import FashionFile
 
 def index(request):
     template = loader.get_template('index.html')
@@ -13,13 +13,16 @@ def upload_file(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            with open('public/' + request.FILES['file'].name, 'wb+') as destination:
-                for chunk in request.FILES['file'].chunks():
+            file = request.FILES['file']
+            with open('fashion/static/' + file.name, 'wb+') as destination:
+                for chunk in file.chunks():
                     destination.write(chunk)
+            FashionFile(file_name=file.name, type='anime').save()
             return HttpResponseRedirect('result')
     return HttpResponseRedirect('/fashion')
 
 
 def result(request):
     template = loader.get_template('result.html')
-    return HttpResponse(template.render({}, request))
+    files = FashionFile.objects.all()
+    return HttpResponse(template.render({'files': files}, request))
