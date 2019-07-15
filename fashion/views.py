b8a840a97e634b0cc3f3ae7e273d486cb75a969a
Amelie Certin <amelie.certin@gmail.com>
2019-07-15 15:33:45 +0200

    Upload file

--- fashion/views.py
+++ fashion/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from .forms import UploadFileForm
 
 
 def index(request):
     template = loader.get_template('index.html')
+    return HttpResponse(template.render({}, request))
+
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            with open('public/' + request.FILES['file'].name, 'wb+') as destination:
+                for chunk in request.FILES['file'].chunks():
+                    destination.write(chunk)
+            return HttpResponseRedirect('result')
+    return HttpResponseRedirect('/fashion')
+
+
+def result(request):
+    template = loader.get_template('result.html')
     return HttpResponse(template.render({}, request))
